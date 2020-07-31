b0c7eb421114c6fbc5d77b73f57f37259cad8080
Mitchell Wilson <m_w211@txstate.edu>
2020-07-31 14:18:49 -0500

    Added more test cases

--- data_test.py
+++ data_test.py
@@ -1,4 +1,6 @@
 from model import Model
+from view import View
+from controller import Controller
 import pytest
 
 
@@ -18,3 +20,17 @@ def test_years():
     model.get_years()
     years = ["ALL", "2019", "2018", "2017"]
     assert model.years == years
+
+def test_sort():
+    model = Model()
+    model.read_data("data/Sales_Data.csv")
+    model.sort_sd_date()
+    failures = 0
+    for x in range(len(model.sales_data)):
+        if x < len(model.sales_data)-1:
+            if model.sales_data[x][4] > model.sales_data[x+1][4]:
+                failures = failures + 1
+    assert failures == 0
+
+
+
